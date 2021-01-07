c10fe3fbfff230e2f5573395ce17a91fcd12d660
raulmanzas <raulmanzas@gmail.com>
2021-01-07 08:10:31 -0300

    adds apache storm topologies project

--- apache_flume/agent-runner.sh
+++ apache_flume/agent-runner.sh
@@ -6,15 +6,15 @@
 FLUME_PATH=/opt/flume
 
 # Path to a .conf file that defines an agent. It should be one of the available in /agents directory
-AGENT_CONFIG_PATH=$1
+AGENT_CONF_FILE=$1
 
 # Name of the agent to run, matching the name defined on .conf file
 AGENT_NAME=$2
 
-echo ">>> Starting agent: '${AGENT_NAME}' from '${AGENT_CONFIG_PATH}'..."
+echo ">>> Starting agent: '${AGENT_NAME}' from '${AGENT_CONF_FILE}'..."
 
 $FLUME_PATH/bin/flume-ng agent \
     --conf $FLUME_PATH/conf \
-    --conf-file $AGENT_CONFIG_PATH/ \
+    --conf-file /flume/agents/$AGENT_CONF_FILE/ \
     --name $AGENT_NAME \
     -Dflume.root.logger=INFO,console \
---
+++ apache_storm/topologies_project/src/main/kotlin/topologies/demo/App.kt
@@ -0,0 +1,5 @@
+package topologies.demo
+
+fun main() {
+    println("Run topology")
+}
